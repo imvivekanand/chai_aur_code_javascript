fde9953bc1d6e71f59e321bc3e2673f3d8c52290
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2023-10-15 00:56:39 +0530

    variables

---
+++ 4_Variables.js
@@ -0,0 +1,8 @@
+const username = "vivekanand";
+let realName = "Vivek Anand";
+var password = "123456";
+state = "Bihar"
+let country;
+
+console.table([username, realName, password, state, country])
+console.log(username, realName, password, state, country)
