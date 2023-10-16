f1e62f14c883b59e57b612d79bb4e6e87d09fb9f
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2023-10-16 15:13:34 +0530

    Comparision of datatypes

---
+++ 7_Operations.js
@@ -0,0 +1,34 @@
+// let value = 3;
+// let negValue = -value
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3); //Power
+// console.log(2/3);
+// console.log(2%3);
+
+// console.log(2 + "3");
+// console.log("2" + 3);
+// console.log("2" + 3 + 8);
+// console.log(2 + 3 + "8");
+
+// console.log(3 + 4 * 5 % 3); //Don't write code like this, always use parenthesis. 
+
+
+//____________________________________________________________________
+//Don't write code like this.
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2+2
+//_____________________________________________________________________
+
+let gameCounter = 100
+gameCounter++;
+console.log(gameCounter);
+
+
+//Study about prifix and postfix on MDN.                                  
+
---
+++ 8_Comparision.js
@@ -0,0 +1,25 @@
+//Comparision of datatypes
+
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 < 1);
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+// console.log("2" > 1);
+// console.log("02" > 1);
+
+
+//_________________________________________
+// Avoid these types of comparision
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined == 0);
+console.log(undefined > 0);
+console.log(undefined < 0);
+
+//_________________________________________
+
+console.log("2" === 2);
