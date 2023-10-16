bb3b574c3847db37862079660e25b24d29818576
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2023-10-16 12:10:37 +0530

    conversion of data types

---
+++ 6_DataTypeConversion.js
@@ -0,0 +1,25 @@
+// let score = "88";
+
+// console.log(typeof score)
+
+// let valueInNumber = Number(score);
+
+// console.log(valueInNumber);
+
+// const a = ""
+// const b = "Vivek"
+// const c = true;
+// const d = false;
+
+// const e = Boolean(a)
+// const f = Boolean(b)
+// const g = Boolean(c)
+// const h = Boolean(d)
+
+// console.table([e, f, g, h])
+
+let number = 8;
+
+let stringNumber = String(number)
+console.log(stringNumber);
+console.log(typeof stringNumber);
