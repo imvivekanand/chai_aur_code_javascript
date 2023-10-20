664b450942fc770a4edf18cb49f62a20dc70d0d0
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2023-10-20 23:01:43 +0530

    objects part 1

--- basics_2/03_objects.js
+++ basics_2/03_objects.js
@@ -1,15 +1,35 @@
-//Singleton 
+//Singleton - Made through constructor.
 
 //Object Literals
+
+const mySym = Symbol("Key1")
 
 const jsUser = {
     name: "Vivek Anand",
     age: 27,
-    location: "Sitamarhi",
+    [mySym]: "myKey1", //Without [] in key, we cannot use symbol data type
+    "last location": "Sitamarhi",
     email: "vivekanand@onmail.com",
     isLoggedIN: "false",
     lastLoginDays: ["Monday", "Saturday"]
 }
 
-console.log(jsUser.name);
-console.log(jsUser["location"]);
+// console.log(jsUser.name); //Note: If we have space in key. We cannot use the dot notation. So always use square notation [].
+// console.log(jsUser["last location"]);
+// console.log(jsUser);
+// console.log(jsUser.mySym);
+
+//how to overide the values of object.
+jsUser.email= "vivek"
+
+// Object.freeze(jsUser) //We cannot make any changes after freezing the object.
+
+jsUser.greeting = function(){
+    console.log("Hello User");
+}
+jsUser.greeting2 = function(){
+    console.log(`Hello! ${this.name}`);
+}
+
+jsUser.greeting()
+jsUser.greeting2()
