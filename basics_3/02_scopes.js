69588a5189142f41f2798e24b0dcb03993edf222
VIVEK ANAND <47303600+imvivekanand@users.noreply.github.com>
2023-10-21 14:05:11 +0530

    scope

---
+++ basics_3/02_scopes.js
@@ -0,0 +1,26 @@
+
+
+
+
+
+
+
+
+
+//function - It can be called before declaring the function.
+
+function one(){
+    const username = "hitesh"
+}
+
+//function expression - It cannot be declared before declaring the function.
+
+const two = function(){
+    return "two";
+}
+
+//arrow functions
+
+const arrFunc = () => {
+    
+}
